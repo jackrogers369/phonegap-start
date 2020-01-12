3c360bbcd4558d02562031f97af8cbeda6d11f29
jackrogers369 <55839281+jackrogers369@users.noreply.github.com>
2020-01-12 12:44:00 +0000

    update

---
+++ www/my app/map.js
@@ -0,0 +1,19 @@
+
+function myMap() {
+var mapProp= {
+  center:new google.maps.LatLng(52.18935, -2.22001),
+  zoom:15,
+};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+/*
+var marker = new google.maps.Marker({position: myCenter});
+
+marker.setMap(map);
+*/
+
+
+}
+
+
+
