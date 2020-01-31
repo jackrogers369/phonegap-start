1bdb9459d6383e3df8aff4416e6ef4b7a7098b46
jackrogers369 <55839281+jackrogers369@users.noreply.github.com>
2020-01-31 15:34:33 +0000

    uyiyb

--- www/my app/index.js
+++ www/my app/index.js
@@ -75,3 +75,25 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
