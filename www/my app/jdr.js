5bb4de919fdc6e0db58698a5a9bde8f9a26748a8
jackrogers369 <55839281+jackrogers369@users.noreply.github.com>
2020-03-06 13:09:43 +0000

    knm

--- www/my app/jdr.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $("#flip").click(function(){
-    $("#panel").slideDown("slow");
-  });
-});
-
-var str = $('.number').html();
-str.replace('&nbsp;', '');
-$('.number').html(str);
